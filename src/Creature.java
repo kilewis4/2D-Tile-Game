05b19414db50a9316085dc57b9df0b044698fa0b
Kaeden Lewis <lewiski@appstate.edu>
2024-12-29 22:39:22 -0500

    Creature sizing

--- src/Creature.java
+++ src/Creature.java
@@ -2,11 +2,66 @@ package src;
 
 public abstract class Creature extends Entity
 {
-    protected int health;
+    public static final int DEFAULT_HEALTH = 10;
+    public static final float DEFAULT_SPEED = 3.0f;
+    public static final int DEFAULT_CREATURE_WIDTH = 64, DEFAULT_CREATURE_HEIGHT = 64;
 
-    public Creature(float x, float y)
+    protected int health;
+    protected float speed;
+    protected float xMove, yMove;
+
+    public Creature(float x, float y, int width, int height)
     {
-        super(x, y);
-        health = 10;
+        super(x, y, width, height);
+        health = DEFAULT_HEALTH;
+        speed = DEFAULT_SPEED;
+        xMove = 0;
+        yMove = 0;
+    }
+
+    public void move()
+    {
+        x += xMove;
+        y += yMove;
+    }
+
+    public int getHealth()
+    {
+        return health;
+    }
+
+    public void setHealth(int health)
+    {
+        this.health = health;
+    }
+
+    public float getSpeed()
+    {
+        return speed;
+    }
+
+    public void setSpeed(float speed)
+    {
+        this.speed = speed;
+    }
+
+    public float getXMove()
+    {
+        return xMove;
+    }
+
+    public void setXMove(float xMove)
+    {
+        this.xMove = xMove;
+    }
+
+    public float getYMove()
+    {
+        return yMove;
+    }
+
+    public void setYMove(float yMove)
+    {
+        this.yMove = yMove;
     }
 }
